ef31c3594b7cd9ed2bd64edd7a2785727a04778f
Blaz Pahor <blaz.pahor3@gmail.com>
2026-04-07 13:56:07 +0200

    fix unused imports

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
-import { getTransactions } from "../../lib/storage";
 import { redirect } from "next/navigation";
 
 export default function Home() {
